8475a57f11926e41d1cb50d61659804ad37d2b79
Kelvin Hart <Kelvin Hart>
2020-09-26 21:23:37 +0300

    update

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+import java.util.scanner;
+public class helloworld {
+public static void main(String[] args) {
+  Scanner reader = new Scanner(system.in);
+  System.out.print("Enter a number: ");
+  int number = reader .nextInt();
+  System.out.println("you entered: " + number);
+  }
+}
